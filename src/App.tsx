5cb44928f1849e035c28f460f305404ff10d81b6
serejs <sokolovserej@gmail.com>
2024-01-05 10:55:20 +0300

    Default front entrance

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {InputPanel} from "./panel/InputPanel";
+import {DiagramView} from "./DiagramView";
+import {MathComponent} from "mathjax-react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [connections, setConnections] = useState([
+        [0, 1, 0],
+        [0, 0, 1],
+        [0, 0, 0],
+    ]);
+
+    const [naturalMapping, setNaturals] = useState([])
+
+    return (
+        <div className="App section">
+            <div className={"columns"}>
+                <div className={"column"}><InputPanel size={10} setNatural={setNaturals} setConnections={setConnections}/></div>
+                <div className={"column"}><DiagramView connections={connections}/></div>
+            </div>
+
+            <div className="table-container">
+                <table className="table">
+                    <tbody>
+                    {(() => {
+                        return <>
+                            <tr>
+                                {(() => {
+                                    const arr = [];
+                                    for (let i = 1; i <= connections.length; i++) {
+                                        arr.push(<td>
+                                            <MathComponent tex={String.raw`a_{${i}}`}/>
+                                        </td>);
+                                    }
+                                    return arr;
+                                })()}
+                            </tr>
+                            <tr>
+                                {(() => {
+                                    const arr = [];
+                                    for (let i = 0; i < connections.length; i++) {
+                                        arr.push(
+                                            <td>{naturalMapping! && naturalMapping.length === connections.length
+                                                ? naturalMapping[i] : "?"}</td>);
+                                    }
+                                    return arr;
+                                })()}
+                            </tr>
+                        </>
+                    })()}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
 
 export default App;
